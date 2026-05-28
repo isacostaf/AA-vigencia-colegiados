06b6aa66a64b7ca007116277188cf5eb6d7f2895
isacostaf <iisabellecostaf@gmail.com>
2026-05-28 16:02:33 -0300

    corrige fluxo upload render4

--- src/browserService.js
+++ src/browserService.js
@@ -1,13 +1,22 @@
 const puppeteer = require('puppeteer');
 
 async function createBrowser() {
-    return await puppeteer.launch({
+
+    const browser = await puppeteer.launch({
+
         headless: true,
+
+        executablePath:
+            process.env.PUPPETEER_EXECUTABLE_PATH ||
+            '/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
+
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
         ]
     });
+
+    return browser;
 }
 
 module.exports = {
