683d9054904298ccdcffbd90f37f3923b347c2e2
isacostaf <iisabellecostaf@gmail.com>
2026-05-28 16:05:23 -0300

    corrige fluxo upload render5

--- src/browserService.js
+++ src/browserService.js
@@ -6,9 +6,7 @@ async function createBrowser() {
 
         headless: true,
 
-        executablePath:
-            process.env.PUPPETEER_EXECUTABLE_PATH ||
-            '/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
+        executablePath: puppeteer.executablePath(),
 
         args: [
             '--no-sandbox',
