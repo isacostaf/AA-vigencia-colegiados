2b8b2f58b702764b2e3dbd96242c2a52a446de58
isacostaf <iisabellecostaf@gmail.com>
2026-05-28 16:20:11 -0300

    corrige fluxo upload render8

--- src/browserService.js
+++ src/browserService.js
@@ -1,10 +1,37 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
 
 async function createBrowser() {
+
+    const cacheDir = '/opt/render/.cache/puppeteer/chrome';
+
+    let executablePath = null;
+
+    if (fs.existsSync(cacheDir)) {
+
+        const versions = fs.readdirSync(cacheDir);
+
+        if (versions.length > 0) {
+
+            const chromeFolder = versions[0];
+
+            executablePath = path.join(
+                cacheDir,
+                chromeFolder,
+                'chrome-linux64',
+                'chrome'
+            );
+        }
+    }
+
+    console.log('Chrome path:', executablePath);
 
     return await puppeteer.launch({
 
         headless: true,
+
+        executablePath,
 
         args: [
             '--no-sandbox',
